7ee202cbc49d9adf887d3534ffda748055582ce6
Nerea <neresaas@gmail.com>
2024-03-10 18:31:05 +0100

    POST /orders/:id/items

--- routers/routerOrders.js
+++ routers/routerOrders.js
@@ -40,4 +40,30 @@ routerOrders.post('/', async (req, res) => {
     res.json({inserted: insertedOrder})
 });
 
+routerOrders.post('/:idOrder/items', async (req, res) => {
+    let idOrder = req.params.idOrder
+    if (idOrder == undefined) {
+        return res.status(400).json({error: 'no idOrder'})
+    }
+    let idItem = req.body.idItem
+    if (idItem == undefined) {
+        return res.status(400).json({error: 'no idItem in body'})
+    }
+    let units = req.body.units
+    if (units == undefined) {
+        return res.status(400).json({error: 'no units in body'})
+    }
+    units = parseInt(units)
+
+    database.connect()
+    let itemsInOrder = await database.query('SELECT * FROM orders_items WHERE idOrder = ? AND idItem = ?', [idOrder, idItem])
+    if (itemsInOrder.length > 0) {
+        return res.status(400).json({error: 'item alredy exists '})
+    }
+
+    await database.query('INSERT INTO orders_items (idOrder, idItem, units) VALUES (?, ?, ?)', [idOrder, idItem, units])
+    database.disConnect()
+    res.json({inserted: true})
+});
+
 module.exports = routerOrders;
